af524ac5068f475f51a6805d7f1a6715accc523e
Redouane Maamri <maamriredouane9@gmail.com>
2025-07-11 10:56:37 +0100

    Add CI/CD GitHub Action with Cypress

--- src/Produits/Products.jsx
+++ src/Produits/Products.jsx
@@ -3,30 +3,27 @@ import "./products.css";
 import { useTranslation } from 'react-i18next';
 import { Link } from "react-router-dom";
 
-
 export default function Produits() {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
   const produits = [
     { img: '/pic1.jpeg', nom: 'Panneau Solaire Photovoltaïque', reference: 'PSPV-300', description: 'Panneau solaire monocristallin 300W' },
     { img: '/pic2.jpeg', nom: 'Onduleur Solaire', reference: 'ONDL-5K', description: 'Onduleur 5kW pour système solaire' },
     { img: '/pic3.jpeg', nom: 'Batterie Lithium', reference: 'BAT-LI-10K', description: 'Batterie lithium 10kWh' },
-  
-    // Remplacement des 3 derniers
     { img: '/Onduleur/onduleur1.jpeg', nom: 'Onduleur 3000W', reference: 'ONDL-3K', description: 'Onduleur 3000W pour système solaire' },
     { img: '/CablesSolaire/cable3.png', nom: 'Câble souple', reference: 'CABL-SPL', description: 'Pour installation flexible' },
     { img: '/Protection/protection2.jpeg', nom: 'Parafoudre', reference: 'PARA-01', description: 'Protection contre les surtensions dues à la foudre' }
   ];
-  
 
   return (
     <section id="produits" className="Produits-container">
       <h2><span>{t("nav_spanproductttile")}</span>{t("nav_titlepage")}</h2>
+
       <div className="img-container">
         <ul>
           {produits.map((produit, index) => (
             <li key={index} className="produit-card">
-              <img loading='lazy' src={produit.img} alt={produit.nom} className="produit-image" />
+              <img loading="lazy" src={produit.img} alt={produit.nom} className="produit-image" />
               <h3>{produit.nom}</h3>
               <p><strong>Référence :</strong> {produit.reference}</p>
               <p>{produit.description}</p>
@@ -34,10 +31,12 @@ export default function Produits() {
           ))}
         </ul>
 
-        <Link to="/productsCatalogue"><button>Découvrir tous les produits</button></Link>
+        <Link to="/productsCatalogue">
+          <button>Découvrir tous les produits</button>
+        </Link>
       </div>
 
-      <hr className="full-products"  />
+      <hr className="full-products" />
     </section>
   );
 }
